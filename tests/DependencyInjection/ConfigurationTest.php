41bf730fec628cee596df325052f4205e973466a
Dawid Spiechowicz <spiechu@gmail.com>
2017-08-02 21:50:27 +0200

    add basic tests

---
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiechu\SymfonyCommonsBundle\Test\DependencyInjection;
+
+use Spiechu\SymfonyCommonsBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Processor;
+
+class ConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Configuration
+     */
+    protected $configuration;
+
+    /**
+     * @var Processor
+     */
+    protected $processor;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->configuration = new Configuration();
+        $this->processor = new Processor();
+    }
+
+    public function testConfigurationLoadsProperlyOnEmptyConfig(): void
+    {
+        $config = $this->processor->processConfiguration(
+            $this->configuration,
+            []
+        );
+
+        self::assertNotEmpty($config['get_method_override']);
+    }
+}
