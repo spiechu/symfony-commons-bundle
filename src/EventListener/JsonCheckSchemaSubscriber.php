90a65d61a902cf3e161d66dc958e532f605b5924
Dawid Spiechowicz <spiechu@gmail.com>
2017-08-03 00:11:06 +0200

    json schema validator

--- src/EventListener/JsonCheckSchemaSubscriber.php
+++ src/EventListener/JsonCheckSchemaSubscriber.php
@@ -6,10 +6,21 @@ namespace Spiechu\SymfonyCommonsBundle\EventListener;
 
 use Spiechu\SymfonyCommonsBundle\Event\ResponseSchemaCheck\CheckRequest;
 use Spiechu\SymfonyCommonsBundle\Event\ResponseSchemaCheck\Commands;
+use Spiechu\SymfonyCommonsBundle\Service\JsonSchemaValidator;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class JsonCheckSchemaSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var JsonSchemaValidator
+     */
+    protected $jsonSchemaValidator;
+
+    public function __construct(JsonSchemaValidator $jsonSchemaValidator)
+    {
+        $this->jsonSchemaValidator = $jsonSchemaValidator;
+    }
+
     /**
      * {@inheritdoc}
      */
