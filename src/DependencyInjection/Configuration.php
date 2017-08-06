8fb464549320ef2c8b38d0f10eae201ae28351cf
Dawid Spiechowicz <spiechu@gmail.com>
2017-08-06 16:05:52 +0200

    enable pipelines

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -6,7 +6,6 @@ use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-
 class Configuration implements ConfigurationInterface
 {
     /**
