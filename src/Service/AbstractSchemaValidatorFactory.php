1058f27a7bc088ce034d17d9729ac5b1b35922fc
Dawid Spiechowicz <spiechu@gmail.com>
2017-11-12 12:46:13 +0100

    abstract out common path of json schema validator factory

---
+++ src/Service/AbstractSchemaValidatorFactory.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiechu\SymfonyCommonsBundle\Service;
+
+use Symfony\Component\Config\FileLocatorInterface;
+
+abstract class AbstractSchemaValidatorFactory
+{
+    /**
+     * @var FileLocatorInterface
+     */
+    protected $fileLocator;
+
+    /**
+     * @var array
+     */
+    protected $registeredSchemas;
+
+    /**
+     * @param FileLocatorInterface $fileLocator
+     */
+    public function __construct(FileLocatorInterface $fileLocator)
+    {
+        $this->fileLocator = $fileLocator;
+
+        $this->registeredSchemas = [];
+    }
+
+    /**
+     * @param string $id
+     * @param string $schemaResourceLocation
+     *
+     * @throws \RuntimeException When schema with provided $id is already registered
+     *
+     * @return self
+     */
+    public function registerSchema(string $id, string $schemaResourceLocation): self
+    {
+        if ($this->hasSchema($id)) {
+            throw new \RuntimeException(sprintf('Schema with id "%s" already registered.', $id));
+        }
+
+        $this->registeredSchemas[$id] = $schemaResourceLocation;
+
+        return $this;
+    }
+
+    /**
+     * @param string $id
+     *
+     * @return bool
+     */
+    public function hasSchema(string $id): bool
+    {
+        return array_key_exists($id, $this->registeredSchemas);
+    }
+
+    /**
+     * @param string $id
+     * @throws \RuntimeException When schema with id "%s" is not registered
+     */
+    protected function assertHasSchema(string $id): void
+    {
+        if (!$this->hasSchema($id)) {
+            throw new \RuntimeException(sprintf('Schema with id "%s" is not registered.', $id));
+        }
+    }
+}
--- src/Service/JsonSchemaValidatorFactory.php
+++ src/Service/JsonSchemaValidatorFactory.php
@@ -8,58 +8,9 @@ use JsonSchema\SchemaStorage;
 use JsonSchema\Uri\UriRetriever;
 use JsonSchema\Validator;
 use Symfony\Component\Config\Exception\FileLocatorFileNotFoundException;
-use Symfony\Component\Config\FileLocatorInterface;
 
-class JsonSchemaValidatorFactory
+class JsonSchemaValidatorFactory extends AbstractSchemaValidatorFactory
 {
-    /**
-     * @var FileLocatorInterface
-     */
-    protected $fileLocator;
-
-    /**
-     * @var array
-     */
-    protected $registeredSchemas;
-
-    /**
-     * @param FileLocatorInterface $fileLocator
-     */
-    public function __construct(FileLocatorInterface $fileLocator)
-    {
-        $this->fileLocator = $fileLocator;
-        $this->registeredSchemas = [];
-    }
-
-    /**
-     * @param string $id
-     * @param string $schemaResourceLocation
-     *
-     * @throws \RuntimeException When schema with provided $id is already registered
-     *
-     * @return JsonSchemaValidatorFactory
-     */
-    public function registerSchema(string $id, string $schemaResourceLocation): self
-    {
-        if ($this->hasSchema($id)) {
-            throw new \RuntimeException(sprintf('Schema with id "%s" already registered.', $id));
-        }
-
-        $this->registeredSchemas[$id] = $schemaResourceLocation;
-
-        return $this;
-    }
-
-    /**
-     * @param string $id
-     *
-     * @return bool
-     */
-    public function hasSchema(string $id): bool
-    {
-        return array_key_exists($id, $this->registeredSchemas);
-    }
-
     /**
      * @param string $id
      *
@@ -71,9 +22,7 @@ class JsonSchemaValidatorFactory
      */
     public function getValidator(string $id): JsonSchemaValidator
     {
-        if (!$this->hasSchema($id)) {
-            throw new \RuntimeException(sprintf('Schema with id "%s" is not registered.', $id));
-        }
+        $this->assertHasSchema($id);
 
         if (!$this->registeredSchemas[$id] instanceof JsonSchemaValidator) {
             $this->registeredSchemas[$id] = new JsonSchemaValidator($this->createSchema($id), new Validator());
