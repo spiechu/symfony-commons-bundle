cc638bc31592dc9b65b4ec50e7ce859350e0967e
Dawid Spiechowicz <spiechu@gmail.com>
2017-11-30 20:57:46 +0100

    add twig bundle for SF4 tests

--- tests/Functional/app/FosRestBundleIncluded/bundles.php
+++ tests/Functional/app/FosRestBundleIncluded/bundles.php
@@ -6,11 +6,13 @@ use Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle;
 use Spiechu\SymfonyCommonsBundle\SpiechuSymfonyCommonsBundle;
 use Spiechu\SymfonyCommonsBundle\Test\Functional\Bundle\TestBundle\TestBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
 
 return [
     new FrameworkBundle(),
     new SpiechuSymfonyCommonsBundle(),
     new SensioFrameworkExtraBundle(),
+    new TwigBundle(),
     new TestBundle(),
     new FOSRestBundle(),
     new JMSSerializerBundle(),
